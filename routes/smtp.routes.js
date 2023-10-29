d46cb8db5e58353371d3d305e1da193735c97826
rohitkokkula <rohitkokkula123@gmail.com>
2023-10-29 19:21:49 +0530

    first commit

--- routes/smtp.routes.js
+++ routes/smtp.routes.js
@@ -18,18 +18,19 @@ const transporter = nodemailer.createTransport({
 });
 transporter.verify().then(console.log).catch(console.error);
 
-async function sendMails(emails,data) {
+async function sendMails(emails, data) {
   let responses = [];
   let mails = [];
   let i = 0;
-  const path = "../uploads/";
-  const filenames=await fs.readdir(path);
-  const files=filenames.map(filename => fs.readFile(path + filename));
-  const attachments=files.map(file => {
-    return {filename: file.originalname, content: file.buffer} 
+  const path = "uploads/";
+  const filenames = fs.readdirSync(path);
+  const files = filenames.map(filename => fs.readFileSync(path + filename));
+  const attachments = files.map(file => {
+    return { filename: filenames[i++], content: file }
   });
-  const from=process.env.SMTP_FROM;
-  mails = emails.map(to => transporter.sendMail({from,to,subject:data[i].subject,html:data[i].body,attachments}));
+  const from = process.env.SMTP_FROM;
+  i = 0;
+  mails = emails.map(to => transporter.sendMail({ from, to, subject: data[i].subject, html: data[i].body, attachments }));
   responses = await Promise.all(mails);
   return responses.length;
 }
@@ -37,17 +38,17 @@ async function sendMails(emails,data) {
 
 router.use(auth.checkToken);
 
-router.get("/",upload.none(), async (req, res) => {
+router.get("/", upload.none(), async (req, res) => {
   res.status(200).send("hello");
 });
 
-router.post("/",upload.none(), async (req, res, next) => {
-  const {emails,data}=req.body;
-  if(!emails || !data){
+router.post("/", upload.none(), async (req, res, next) => {
+  const { emails, data } = req.body;
+  if (!emails || !data) {
     res.status(201).json("Please provide mail data");
   }
   try {
-    const i = await sendMails(emails,data);
+    const i = await sendMails(emails, data);
     res.status(201).json(i + " Mails Have Been Sent Successfully");
   } catch (e) {
     next(e);
@@ -56,7 +57,7 @@ router.post("/",upload.none(), async (req, res, next) => {
 
 router.post("/attachments", upload.array("files"), async (req, res, next) => {
   try {
-    res.status(201).json(req.files.length+" Files have been saved successfully");
+    res.status(201).json(req.files.length + " Files have been saved successfully");
   } catch (e) {
     next(e);
   }
