056951b789a42b2fd6c95a09671327745688c2b9
Kokkula Shivakumar Mallesh <kokkulashivakumar1@gmail.com>
2024-07-15 00:57:18 +0530

    first commit

--- middlewares/error.js
+++
@@ -1,5 +0,0 @@
-module.exports = (err, req, res, next) => {
-    let { statusCode, message } = err;
-    console.log(err);
-    res.status(statusCode || 500).json(message ||  "Internal Server Error");
-}
